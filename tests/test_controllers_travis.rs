a7cba648c928d9011247a56dde575052e570ef71
charles 🔥 <charleschong@Charless-MacBook-Pro.local>
2020-04-19 16:28:35 -0700

    quick deploy

--- tests/test_controllers_travis.rs
+++ tests/test_controllers_travis.rs
@@ -18,30 +18,17 @@ fn test_set_current_directory() {
     assert_eq!(travis::set_current_directory(), true);
 }
 
-// #[test]
-// fn test_tar_compress_secrets_directory() {
-//     assert_eq!(travis::set_current_directory(), true);
-//     assert_eq!(travis::tar_compress_secrets_directory(), true);
-// }
+#[test]
+fn test_tar_compress_secrets_directory() {
+    assert_eq!(travis::tar_compress_secrets_directory(), true);
+}
 
-// #[test]
-// fn test_tar_decompress_secrets_directory() {
-//     assert_eq!(travis::set_current_directory(), true);
-//     assert_eq!(travis::tar_compress_secrets_directory(), true);
-//     assert_eq!(travis::tar_decompress_secrets_directory(), true);
-// }
+#[test]
+fn test_tar_decompress_secrets_directory() {
+    assert_eq!(travis::tar_decompress_secrets_directory(), true);
+}
 
-// #[test]
-// fn test_encrypt_tar_secrets() {
-//     assert_eq!(travis::set_current_directory(), true);
-//     assert_eq!(travis::tar_compress_secrets_directory(), true);
-//     assert_eq!(travis::encrypt_tar_secrets(), true);
-// }
-
-// #[test]
-// fn test_add_openssl_cmd() {
-//     assert_eq!(travis::set_current_directory(), true);
-//     assert_eq!(travis::tar_compress_secrets_directory(), true);
-//     assert_eq!(travis::encrypt_tar_secrets(), true);
-//     assert_eq!(travis::add_openssl_cmd(), true);
-// }
+#[test]
+fn test_encrypt_tar_secrets() {
+    assert_eq!(travis::encrypt_tar_secrets(), true);
+}
