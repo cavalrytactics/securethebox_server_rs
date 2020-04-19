71cb73348d35f8c3dbca590be2844b17b5b3526c
charles 🔥 <charleschong@Charless-MacBook-Pro.local>
2020-04-19 16:25:29 -0700

    quick deploy

--- tests/test_controllers_travis.rs
+++ tests/test_controllers_travis.rs
@@ -5,22 +5,18 @@ use securethebox_server_rs::controllers::travis;
 
 #[test]
 fn test_decrypt_tar_secrets() {
-    // assert_eq!(travis::set_current_directory(), true);
-    // assert_eq!(travis::tar_compress_secrets_directory(), true);
-    // assert_eq!(travis::encrypt_tar_secrets(), true);
     assert_eq!(travis::decrypt_tar_secrets(), true);
-    // assert_eq!(travis::tar_decompress_secrets_directory(), true);
 }
 
-// #[test]
-// fn test_set_file_name() {
-//     assert_eq!(travis::set_file_name("test"), true);
-// }
+#[test]
+fn test_set_file_name() {
+    assert_eq!(travis::set_file_name("test"), true);
+}
 
-// #[test]
-// fn test_set_current_directory() {
-//     assert_eq!(travis::set_current_directory(), true);
-// }
+#[test]
+fn test_set_current_directory() {
+    assert_eq!(travis::set_current_directory(), true);
+}
 
 // #[test]
 // fn test_tar_compress_secrets_directory() {
